686c6cbd2fb5ff5053e4875b3243927a2a10ace1
정소민 <yjk70702002@gmail.com>
2024-04-02 21:32:54 +0900

    Feat: Controller 인터페이스 생성

--- src/main/java/jwp/controller/LogoutController.java
+++ src/main/java/jwp/controller/LogoutController.java
@@ -1,20 +1,15 @@
 package jwp.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
-@WebServlet("/user/logout")
-public class LogoutController extends HttpServlet {
+public class LogoutController implements Controller {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         HttpSession session = req.getSession();
         //세션 데이터 삭제
         session.removeAttribute("user");
-        resp.sendRedirect("/");
+        return "redirect:/";
     }
 }
